346cf5cd7ce7cca3c236ff94b7fe1f34ff476dcb
Abeznos <abeznos@yandex.ru>
2019-01-11 15:15:39 +0300

    Домашнее задание 3

---
+++ lesson 3/form.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class Form {
+    constructor(id) {
+        this.id = id;
+        this._validateNameInput();
+        this._validatePhoneInput();
+        this._validateEmailInput();
+        this._validateTextareInput();
+    }
+    
+    _validateNameInput() {
+        const nameInput = document.getElementById('exampleInputName');
+        
+        if (/^[a-z]{4,30}$/i.test(nameInput.value)) {
+            nameInput.classList.add('is-valid');
+            nameInput.classList.remove('is-invalid');
+        } else {
+            nameInput.classList.add('is-invalid');
+        }
+        
+    }    
+    
+    _validatePhoneInput() {
+        const phoneInput = document.getElementById('exampleInputPhone');
+        
+        if (/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/gmi.test(phoneInput.value)) {
+            phoneInput.classList.add('is-valid');
+            phoneInput.classList.remove('is-invalid');
+        } else {
+            phoneInput.classList.add('is-invalid');
+        }
+    }    
+    
+    _validateEmailInput() {
+        const emailInput = document.getElementById('exampleInputEmail1');
+
+        if (/^[a-z?\-?\.]+@[a-z?\-?\.]+\.[a-z]{2,3}/gmi.test(emailInput.value)) {
+            emailInput.classList.add('is-valid');
+            emailInput.classList.remove('is-invalid');
+        } else {
+            emailInput.classList.add('is-invalid');
+        }
+    }    
+    
+    _validateTextareInput() {
+        const textareaInput = document.getElementById('exampleTextareaInput');
+
+    }
+    
+    
+} 
